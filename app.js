3f54772fa0acaf22181da29a072bbe143928c3a3
jp <gaines.johnp@gmail.com>
2017-06-12 18:43:50 -0700

    forgot to save...

--- app.js
+++ app.js
@@ -2,4 +2,4 @@
 
 // displays individual store data for my Salmon Cookie Shops
 // represent the store data in a list format on the sales page
-//use object-oriented programming to build this site, so that the site will be more effective and the code will be easier to read and understand
+//use object-oriented programming to build this site, so that the site will be more effective and the code will be easier to read and understand  
