9a1587691a05885a4414229b8eeeaf093ddf0a0a
Firefly4266 <johnathon@Icon-Base.(none)>
2017-06-13 02:54:34 -0700

    populating list

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 'use strict';
 
-// displays individual store data for my Salmon Cookie Shops
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
+// displays individual store data for my Salmon Cookie Shops
 
 let firstAndPike = {
   minCustomers: 23,
   maxCustomers: 65,
-  avgCookies: 6.3
+  avgCookies: 6.3,
+  sumCookies: [],
+  cookies: function(){
+    var amount = Math.round(Math.floor(Math.random() * ((this.max - this.min)) + this.min) * this.aveCookies);
+    this.sumCookies.push(amount);
+    return amount;
+  }
 };
+
+let injection = document.getElementById('firstAndPike');
+
+for (var i = 0; i < hours.length; i++) {
+  var listElement = document.createElement('li');
+  listElement.setAttribute('class', 'hours');
+  listElement.textContent = hours[i] + ': ' + firstAndPike.cookies() + ' cookies sold';
+  injection.appendChild(listElement);
+}
+
+
+
 
 let seaTacAirport = {
   minCustomers: 3,
