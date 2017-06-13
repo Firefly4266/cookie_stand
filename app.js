4e3f45b52830eb8e36243d8b01336ac1c8ceca1f
Firefly4266 <johnathon@Icon-Base.(none)>
2017-06-13 03:50:53 -0700

    Last bit for now. verifying totals.

--- app.js
+++ app.js
@@ -4,28 +4,37 @@ let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 
 // displays individual store data for my Salmon Cookie Shops
 
-let firstAndPike = {
+var firstAndPike = {
+  name: '1st and Pike',
   minCustomers: 23,
   maxCustomers: 65,
   avgCookies: 6.3,
   sumCookies: [],
-  cookies: function(){
-    var amount = Math.round(Math.floor(Math.random() * ((this.max - this.min)) + this.min) * this.aveCookies);
+  cookies: function() { 
+    var amount = Math.round(Math.floor(Math.random() * ((this.maxCustomers - this.minCustomers)) + this.minCustomers) * this.avgCookies);
     this.sumCookies.push(amount);
     return amount;
   }
 };
 
+//use object-oriented programming to build this site, so that the site will be more effective and the code will be easier to read and understand  
+
 let injection = document.getElementById('firstAndPike');
 
-for (var i = 0; i < hours.length; i++) {
+for (var i = 0; i < hours.length; i++) { // represent the store data in a list format on the sales page
   var listElement = document.createElement('li');
   listElement.setAttribute('class', 'hours');
   listElement.textContent = hours[i] + ': ' + firstAndPike.cookies() + ' cookies sold';
   injection.appendChild(listElement);
 }
 
+var total = 0;
+for (var i = 0; i < firstAndPike.sumCookies.length; i++) {
+  total += firstAndPike.sumCookies[i];
+};
 
+console.log(firstAndPike.sumCookies);
+console.log(total);
 
 
 let seaTacAirport = {
@@ -52,5 +61,4 @@ let alkiBeach = {
   avgCookies: 4.6
 };
 
-// represent the store data in a list format on the sales page
-//use object-oriented programming to build this site, so that the site will be more effective and the code will be easier to read and understand  
+
