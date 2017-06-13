683073fce2eb230dd330e98ac087e0c79ae7d6bb
Firefly4266 <johnathon@Icon-Base.(none)>
2017-06-12 23:42:56 -0700

    added some positioning and text to the sales pg

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
 'use strict';
 
 // displays individual store data for my Salmon Cookie Shops
+
+
+var locationPike = {
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgCookies: 6.3
+};
+
+var locationAirport = {
+  minCustomers: 3,
+  maxCustomers: 24,
+  avgCookies: 1.2
+};
+
+var locationCenter = {
+  minCustomers: 11,
+  maxCustomers: 38,
+  avgCookies: 3.7
+};
+
+var locationCapitolHill = {
+  minCustomers: 20,
+  maxCustomers: 38,
+  avgCookies: 2.3
+};
+
+var locationAlki = {
+  minCustomers: 2,
+  maxCustomers: 16,
+  avgCookies: 4.6
+};
+
 // represent the store data in a list format on the sales page
 //use object-oriented programming to build this site, so that the site will be more effective and the code will be easier to read and understand  
